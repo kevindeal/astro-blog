3f2a8d8552f45f0cbbde8ac8435f9a34b1c9fe63
Kevin Deal <kevin@julyfivedesign.com>
2025-02-16 19:32:02 -0500

    pulled data from payload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), tailwind(), react()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Giving up game: Building tomorrow's hustle, Today";
+export const SITE_DESCRIPTION = 'Passing down the cheat codes to life, tech, and building a legacy that lasts.';
